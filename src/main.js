32375d94d68dbabe433f7f9e0b9bfd10a7661bb5
DiaZzzFF <dvsh.dreamer@gmail.com>
2020-07-27 12:03:46 +0300

    7.1.2 Переключит контроллер на работу с моделью

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import BoardComponent from "./components/board.js";
 import BoardController from "./controllers/board.js";
 import FilterComponent from "./components/filter.js";
 import SiteMenuComponent from "./components/site-menu.js";
+import TasksModel from "./models/tasks.js";
 import {generateFilters} from "./mock/filter.js";
 import {generateTasks} from "./mock/task.js";
 import {render, RenderPosition} from "./utils/render.js";
@@ -14,12 +15,14 @@ const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 const filters = generateFilters();
 const tasks = generateTasks(TASK_COUNT);
+const tasksModel = new TasksModel();
+tasksModel.setTasks(tasks);
 
 render(siteHeaderElement, new SiteMenuComponent(), RenderPosition.BEFOREEND);
 render(siteMainElement, new FilterComponent(filters), RenderPosition.BEFOREEND);
 
 const boardComponent = new BoardComponent();
-const boardController = new BoardController(boardComponent);
+const boardController = new BoardController(boardComponent, tasksModel);
 
 render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
 boardController.render(tasks);
