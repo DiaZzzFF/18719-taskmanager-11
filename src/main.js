47be40dbb0e6996f6cfef414379ae8321ed2107e
DiaZzzFF <dvsh.dreamer@gmail.com>
2020-07-28 20:37:19 +0300

    8.1.2 Создаст и подключит API. WIP

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
+import API from "./api.js";
 import BoardComponent from "./components/board.js";
 import BoardController from "./controllers/board.js";
 import FilterController from "./controllers/filter.js";
 import SiteMenuComponent, {MenuItem} from "./components/site-menu.js";
 import StatisticsComponent from "./components/statistics.js";
 import TasksModel from "./models/tasks.js";
-import {generateTasks} from "./mock/task.js";
 import {render, RenderPosition} from "./utils/render.js";
 
-
-const TASK_COUNT = 22;
 
 const dateTo = new Date();
 const dateFrom = (() => {
@@ -18,10 +16,8 @@ const dateFrom = (() => {
   return d;
 })();
 
-const tasks = generateTasks(TASK_COUNT);
+const api = new API();
 const tasksModel = new TasksModel();
-
-tasksModel.setTasks(tasks);
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
@@ -35,7 +31,6 @@ const filterController = new FilterController(siteMainElement, tasksModel);
 render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
 filterController.render();
 render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
-boardController.render();
 render(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
 statisticsComponent.hide();
 
@@ -59,3 +54,9 @@ siteMenuComponent.setOnChange((menuItem) => {
       break;
   }
 });
+
+api.getTasks()
+  .then((tasks) => {
+    tasksModel.setTasks(tasks);
+    boardController.render();
+  });
