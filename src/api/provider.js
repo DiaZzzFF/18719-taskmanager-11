9751fd81d1ae86abb74d899fb91c329edafabe6b
DiaZzzFF <dvsh.dreamer@gmail.com>
2020-07-30 19:50:59 +0300

    9.1.8 Добавит реализацию создания задачи

--- src/api/provider.js
+++ src/api/provider.js
@@ -1,4 +1,5 @@
 import Task from "../models/task.js";
+import {nanoid} from "nanoid";
 
 
 const isOnline = () => {
@@ -28,11 +29,20 @@ export default class Provider {
 
   createTask(task) {
     if (isOnline()) {
-      return this._api.createTask(task);
+      return this._api.createTask(task)
+        .then((newTask) => {
+          this._store.setItem(newTask.id, newTask.toRAW());
+
+          return newTask;
+        });
     }
 
-    // TODO: Реализовать логику при отсутствии интернета
-    return Promise.reject(`offline logic is not implemented`);
+    const localNewTaskId = nanoid();
+    const localNewTask = Task.clone(Object.assign(task, {id: localNewTaskId}));
+
+    this._store.setItem(localNewTask.id, localNewTask.toRAW());
+
+    return Promise.resolve(localNewTask);
   }
 
   updateTask(id, task) {
